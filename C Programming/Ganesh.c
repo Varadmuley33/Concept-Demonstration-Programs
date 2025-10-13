7f40d0363bde95de08b7e3e415becef5c4e62dac
Varad Muley <varad.muley408@gmail.com>
2025-10-13 12:47:51 +0530

    Add files via upload

---
+++ C Programming/Ganesh.c
@@ -0,0 +1,35 @@
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Ganesh.c
+//  Description :   Used to print Jay Ganesh
+//  Author :        Varad Muley
+//  Date :          13/10/2025
+//
+///////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+int main()
+{
+    printf("Jay Ganesh...\n");
+
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+//  This program demonstrates a simple C program that displays "Ganesh" on the screen.
+//  It helps in understanding the following basic C concepts:
+//  1.Preprocessor Directive (#)
+//  Used to include header files before compilation.
+//
+//  2.#include <stdio.h>
+//  Includes the Standard Input/Output library, which provides functions like printf.
+//
+//  3.printf()
+//  Used to display output in a formatted manner.
+//
+//  4.Block-Structured Language
+//  C is a block-structured language, meaning that the code is organized 
+//  into functions and blocks using braces { }.
+//
+//////////////////////////////////////////////////////////////////////////////////////////
