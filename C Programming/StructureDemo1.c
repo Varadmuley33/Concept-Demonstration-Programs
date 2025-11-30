2620c3618433a44c83e93ddd9ffbeee649335f2e
Varad Muley <varad.muley408@gmail.com>
2025-11-30 23:37:33 +0530

    Add files via upload

---
+++ C Programming/StructureDemo1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+struct Demo 
+{
+    int i;
+    float f ;
+    int j;
+
+};
+
+int main ()
+{
+
+    struct Demo obj;
+
+    printf("Size of demo is : %lu\n",sizeof(struct Demo));     //12
+
+    printf("Size of Object is : %lu\n",sizeof(obj));          //12
+
+    return 0;
+
+}
