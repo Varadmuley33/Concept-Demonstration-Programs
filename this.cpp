3c7cae9539847598986b337840a6d31d5cd4de74
Varad Muley <varad.muley408@gmail.com>
2025-12-20 23:09:21 +0530

    Add files via upload

---
+++ this.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+class Arithematic
+{
+    public:
+        int iNo1 ;
+        int iNo2 ;
+
+        Arithematic()
+        {
+            this->iNo1 = 0;
+            this->iNo2 = 0;
+
+        }
+
+        Arithematic(int A,int B)
+        {
+            iNo1 = A;
+            iNo2 = B;
+            
+        }
+
+        int Addition()
+        {
+            int iAns = 0;
+            iAns = iNo1 + iNo2 ;
+            return iAns;
+        }
+
+        int Substraction()
+        {
+            int iAns = 0;
+            iAns = iNo1 - iNo2 ;
+            return iAns;
+        }
+
+
+
+};
+
+int main ()
+{
+
+    Arithematic aobj1(11,10);
+    Arithematic aobj2(21,20);
+
+    int iRet = 0;
+
+    iRet = aobj1.Addition();
+    cout<<"Addition is : "<<iRet<<"\n";
+
+    iRet = aobj1.Substraction();
+    cout<<"Subtraction is : "<<iRet<<"\n";
+
+
+    iRet = aobj2.Addition();
+    cout<<"Addition is : "<<iRet<<"\n";
+
+    iRet = aobj2.Substraction();
+    cout<<"Subtraction is : "<<iRet<<"\n";
+
+    return 0;
+}
