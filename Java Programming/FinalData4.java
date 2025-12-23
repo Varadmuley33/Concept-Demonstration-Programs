4fb8ac1548bc361f0d8f107e0e1f5b6858ae80a3
Varad Muley <varad.muley408@gmail.com>
2025-12-23 23:49:10 +0530

    Update FinalData4.java

--- Java Programming/FinalData4.java
+++ Java Programming/FinalData4.java
@@ -1,7 +1,33 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     FinalData4.java
+//  Description :   Demonstrates initialization of final data member
+//                  using a parameterized constructor in Java.
+//  Author :        Varad Nitin Muley
+//  Date :          24/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    Demo
+//  Description :   Contains normal and final data members initialized
+//                  through a parameterized constructor.
+//
+/////////////////////////////////////////////////////////////////
+
 class Demo
 {
-    public int i;
-    final public int j;
+    public int i;                          // Normal data member
+    final public int j;                   // Final data member
+
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Constructor Name : Demo
+    //  Description      : Initializes data members using parameters.
+    //  Input            : Integer, Integer
+    //
+    /////////////////////////////////////////////////////////////////
 
     public Demo(int a, int b)
     {
@@ -9,6 +35,14 @@ class Demo
         this.j = b;
     }
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    FinalData4
+//  Description :   Entry point class to demonstrate final data
+//                  member initialization via constructor.
+//
+/////////////////////////////////////////////////////////////////
 
 class FinalData4
 {
@@ -23,3 +57,19 @@ class FinalData4
         // dobj.j++;   // Error
     }
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases:
+//
+//  Object Creation : Demo dobj = new Demo(11,21);
+//  Output          : 11
+//                    21
+//
+//  Operation       : dobj.i++
+//  Result          : Allowed
+//
+//  Operation       : dobj.j++
+//  Result          : Compilation Error
+//
+/////////////////////////////////////////////////////////////////
