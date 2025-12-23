cf100ad06c646fb36de95f44bd4ca9194144e178
Varad Muley <varad.muley408@gmail.com>
2025-12-23 23:47:34 +0530

    Update FinalData2.java

--- Java Programming/FinalData2.java
+++ Java Programming/FinalData2.java
@@ -1,14 +1,47 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     FinalData2.java
+//  Description :   Demonstrates initialization of final data members
+//                  at the time of declaration in Java.
+//  Author :        Varad Nitin Muley
+//  Date :          24/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    Demo
+//  Description :   Contains normal and final data members initialized
+//                  during declaration.
+//
+/////////////////////////////////////////////////////////////////
+
 class Demo
 {
-    public int i = 11;
-    final public int j = 21 ;
-    
-    
+    public int i = 11;                     // Normal data member
+    final public int j = 21;               // Final data member
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    FinalData2
+//  Description :   Entry point class to demonstrate final data
+//                  member behavior.
+//
+/////////////////////////////////////////////////////////////////
 
 class FinalData2
 {
-    public static void main (String A[])
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Function Name : main
+    //  Description   : Entry point of Java application
+    //  Input         : String array
+    //  Output        : void
+    //
+    /////////////////////////////////////////////////////////////////
+
+    public static void main(String A[])
     {
         Demo dobj = new Demo();
 
@@ -16,7 +49,22 @@ class FinalData2
         System.out.println(dobj.j);
 
         dobj.i++;
-        //dobj.j++;       //Error
-
+        // dobj.j++;     // Error
     }
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases:
+//
+//  Object Creation : Demo dobj = new Demo();
+//  Initial Output  : 11
+//                    21
+//
+//  Operation       : dobj.i++
+//  Result          : Allowed
+//
+//  Operation       : dobj.j++
+//  Result          : Compilation Error
+//
+/////////////////////////////////////////////////////////////////
