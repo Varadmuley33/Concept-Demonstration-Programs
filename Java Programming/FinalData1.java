cfc7b6f428ea8efc0ba90d55d0bb099d11d01ea0
Varad Muley <varad.muley408@gmail.com>
2025-12-23 23:46:45 +0530

    Enhance documentation in FinalData1.java
    Added detailed comments and documentation for classes and methods.

--- Java Programming/FinalData1.java
+++ Java Programming/FinalData1.java
@@ -1,19 +1,59 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     FinalData1.java
+//  Description :   Demonstrates the use of final data members in Java
+//                  and their behavior after object creation.
+//  Author :        Varad Nitin Muley
+//  Date :          24/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    Demo
+//  Description :   Contains normal and final data members initialized
+//                  inside the constructor.
+//
+/////////////////////////////////////////////////////////////////
+
 class Demo
 {
-    public int i;
-    final public int j;
-    
+    public int i;                          // Normal data member
+    final public int j;                   // Final data member
+
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Constructor Name : Demo
+    //  Description      : Initializes data members of the class.
+    //
+    /////////////////////////////////////////////////////////////////
+
     public Demo()
     {
         this.i = 11;
         this.j = 21;
-
     }
 }
 
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    FinalData1
+//  Description :   Entry point class to demonstrate final keyword.
+//
+/////////////////////////////////////////////////////////////////
+
 class FinalData1
 {
-    public static void main (String A[])
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Function Name : main
+    //  Description   : Entry point of Java application
+    //  Input         : String array
+    //  Output        : void
+    //
+    /////////////////////////////////////////////////////////////////
+
+    public static void main(String A[])
     {
         Demo dobj = new Demo();
 
@@ -21,7 +61,22 @@ class FinalData1
         System.out.println(dobj.j);
 
         dobj.i++;
-        dobj.j++;       //Error
-
+        dobj.j++;       // Error
     }
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases:
+//
+//  Object Creation : Demo dobj = new Demo();
+//  Initial Output  : 11
+//                    21
+//
+//  Operation       : dobj.i++
+//  Result          : Allowed
+//
+//  Operation       : dobj.j++
+//  Result          : Compilation Error
+//
+/////////////////////////////////////////////////////////////////
