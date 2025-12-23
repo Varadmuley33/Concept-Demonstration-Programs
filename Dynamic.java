f5c22f6a82a091878b074197e25322a02957a889
Varad Muley <varad.muley408@gmail.com>
2025-12-23 23:50:39 +0530

    Update Dynamic.java

--- Dynamic.java
+++ Dynamic.java
@@ -1,10 +1,34 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Dynamic.java
+//  Description :   Demonstrates dynamic memory allocation for an
+//                  integer array using user input in Java.
+//  Author :        Varad Nitin Muley
+//  Date :          24/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 import java.util.Scanner;
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    Dynamic
+//  Description :   Allocates memory dynamically for an array and
+//                  checks allocation status.
+//
+/////////////////////////////////////////////////////////////////
 
 class Dynamic 
 {
-    public static void main (String A[])
+    public static void main(String A[])
     {
-        Scanner sobj = new Scanner (System.in);
+        Scanner sobj = new Scanner(System.in);
 
         int length = 0;
         int Arr[];
@@ -14,18 +38,25 @@ class Dynamic
 
         Arr = new int[length];
 
-        if (Arr == null)
+        if(Arr == null)
         {
             System.out.println("Unable to allocate memory");
         }
-        else{
+        else
+        {
             System.out.println("Memory gets successfully allocated");
         }
 
         Arr = null;
         System.gc();
-
-
     }
-
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases:
+//
+//  Input  : 5
+//  Output : Memory gets successfully allocated
+//
+/////////////////////////////////////////////////////////////////
