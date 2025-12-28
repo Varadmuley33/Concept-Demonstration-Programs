d0e05a8496f08a8801837d5780ae1ec52ce68c36
Varad Muley <varad.muley408@gmail.com>
2025-12-28 23:06:11 +0530

    Update ExceptionDemo6.java

--- Java Programming/ExceptionDemo6.java
+++ Java Programming/ExceptionDemo6.java
@@ -1,39 +1,82 @@
-// Division of two numbers 
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     ExceptionDemo6.java
+//  Description :   Demonstrates incorrect ordering of catch blocks
+//                  during division of two user-entered integers.
+//                  Highlights error caused by generic catch before
+//                  specific exception handling.
+//  Author :        Varad Nitin Muley
+//  Date :          27/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 import java.util.*;
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name : ExceptionDemo6
+//  Description : Shows exception handling error due to improper
+//                catch block sequence
+//
+/////////////////////////////////////////////////////////////////
 
 class ExceptionDemo6
 {
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Entry point function
+    //
+    /////////////////////////////////////////////////////////////////
+
     public static void main (String A[])
     {
         Scanner sobj = new Scanner (System.in);
-        int iNo1 = 0 , iNo2 = 0 , iAns = 0;
 
+        int iNo1 = 0;     // First number
+        int iNo2 = 0;     // Second number
+        int iAns = 0;     // Division result
+
+        // Accept first number
         System.out.println("Enter First number ");
         iNo1 = sobj.nextInt();
 
+        // Accept second number
         System.out.println("Enter Second number ");
         iNo2 = sobj.nextInt();
 
         try
         {
             System.out.println("Inside Try block");
-            iAns = iNo1 / iNo2 ;
+            iAns = iNo1 / iNo2;              // Exception prone code
         }
 
-        // Error due to catch sequence 
-        catch (Exception eobj)                              // Genreric catch 
+        // Error due to incorrect catch sequence
+        catch (Exception eobj)               // Generic catch
         {
             System.out.println("Inside Generic catch ");
         }
-        catch (ArithmeticException aobj)                     //Specific catch 
+        catch (ArithmeticException aobj)     // Specific catch (Unreachable)
         {
             System.out.println("Inside catch block");
             System.out.println(aobj);
         }
-        
 
+        // Display result
         System.out.println("Division is : " + iAns);
-
-
     }
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases (Compilation error scenario)
+//
+//  Note : Program demonstrates compile-time error due to
+//         unreachable catch block.
+//
+/////////////////////////////////////////////////////////////////
