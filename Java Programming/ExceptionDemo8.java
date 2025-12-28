012bd65ce25438f8e4659b593e3f4446d2ec612d
Varad Muley <varad.muley408@gmail.com>
2025-12-28 23:07:51 +0530

    Update ExceptionDemo8.java

--- Java Programming/ExceptionDemo8.java
+++ Java Programming/ExceptionDemo8.java
@@ -1,43 +1,89 @@
-// Division of two numbers 
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     ExceptionDemo8.java
+//  Description :   Demonstrates proper use of multiple catch blocks
+//                  along with finally block during integer division.
+//                  Handles both specific and generic exceptions.
+//  Author :        Varad Nitin Muley
+//  Date :          27/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 import java.util.*;
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name : ExceptionDemo8
+//  Description : Performs division using try, catch, and finally
+//                blocks with correct exception handling sequence
+//
+/////////////////////////////////////////////////////////////////
 
 class ExceptionDemo8
 {
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Entry point function
+    //
+    /////////////////////////////////////////////////////////////////
+
     public static void main (String A[])
     {
         Scanner sobj = new Scanner (System.in);
-        int iNo1 = 0 , iNo2 = 0 , iAns = 0;
 
+        int iNo1 = 0;     // First number
+        int iNo2 = 0;     // Second number
+        int iAns = 0;     // Division result
+
+        // Accept first number
         System.out.println("Enter First number ");
         iNo1 = sobj.nextInt();
 
+        // Accept second number
         System.out.println("Enter Second number ");
         iNo2 = sobj.nextInt();
 
         try
         {
             System.out.println("Inside Try block");
-            iAns = iNo1 / iNo2 ;
+            iAns = iNo1 / iNo2;              // Exception prone code
         }
-        
-        catch (ArithmeticException aobj)                     //Specific catch 
+        catch (ArithmeticException aobj)     // Specific catch
         {
             System.out.println("Inside catch block");
             System.out.println(aobj);
         }
-        
-        catch (Exception eobj)                              // Genreric catch 
+        catch (Exception eobj)               // Generic catch
         {
             System.out.println("Inside Generic catch ");
         }
-        finally 
+        finally
         {
             System.out.println("Inside finally block");
         }
-        
 
+        // Display result
         System.out.println("Division is : " + iAns);
-
-
     }
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : 10 2
+//  Output : 5
+//
+//  Input : 8 0
+//  Output : ArithmeticException
+//
+//  Input : 20 4
+//  Output : 5
+//
+/////////////////////////
