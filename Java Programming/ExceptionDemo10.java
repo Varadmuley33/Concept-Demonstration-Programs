f2c4a4abc8480d9127b4ecf6cf3d0830feb69eb6
Varad Muley <varad.muley408@gmail.com>
2025-12-28 22:56:30 +0530

    Update ExceptionDemo10.java

--- Java Programming/ExceptionDemo10.java
+++ Java Programming/ExceptionDemo10.java
@@ -1,34 +1,81 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     ExceptionDemo10.java
+//  Description :   Demonstrates exception handling while accessing
+//                  array elements using user-provided index value.
+//                  Shows the use of try, catch, and finally blocks.
+//  Author :        Varad Nitin Muley
+//  Date :          27/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 import java.util.*;
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name : ExceptionDemo10
+//  Description : Handles runtime exceptions occurring due to
+//                invalid array index access
+//
+/////////////////////////////////////////////////////////////////
 
 class ExceptionDemo10
 {
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Entry point function
+    //
+    /////////////////////////////////////////////////////////////////
+
     public static void main (String A[])
     {
         Scanner sobj = new Scanner (System.in);
-        int Arr[] = {10,20,30,40,50};
 
-        int iIndex = 0, iData = 0 ;
+        int Arr[] = {10, 20, 30, 40, 50};   // Integer array
+        int iIndex = 0;                    // Index value
+        int iData = 0;                     // Element at index
 
+        // Accept index from user
         System.out.println("Enter the index  number ");
         iIndex = sobj.nextInt();
-   
+
         try
         {
             System.out.println("Inside try");
-            iData = Arr[iIndex];                // Exception prone code 
+            iData = Arr[iIndex];            // Exception prone code
         }
         catch(Exception eobj)
         {
             System.out.println("Inside catch");
-            System.out.println(eobj);
+            System.out.println(eobj);       // Display exception details
         }
         finally
         {
             System.out.println("Inside finally");
         }
 
-        System.out.println("Element at that index is  : " +iData);
-
-
-    }        // ---------------------------
+        // Display array element
+        System.out.println("Element at that index is  : " + iData);
+    }       
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : 2
+//  Output : 30
+//
+//  Input : 0
+//  Output : 10
+//
+//  Input : 4
+//  Output : 50
+//
+/////////////////////////////////////////////////////////////////
