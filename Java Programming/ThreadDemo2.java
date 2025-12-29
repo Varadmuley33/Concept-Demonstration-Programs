4f2d4dea1a7584e1543e982eabce5fca580100d1
Varad Muley <varad.muley408@gmail.com>
2025-12-29 23:23:00 +0530

    Add files via upload

---
+++ Java Programming/ThreadDemo2.java
@@ -0,0 +1,12 @@
+class ThreadDemo2
+{
+    public static void main (String A[])
+    {
+        System.out.println("Inside Main method");
+
+        Thread tobj = Thread.currentThread();
+        String name = tobj.getName();
+        
+        System.out.println("Name of current thread is : "+ name);
+    }
+}
