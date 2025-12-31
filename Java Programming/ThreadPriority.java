e449ab2b680721f041dbfe537e95b3854b561848
Varad Muley <varad.muley408@gmail.com>
2025-12-31 21:34:01 +0530

    Update ThreadPriority.java

--- Java Programming/ThreadPriority.java
+++ Java Programming/ThreadPriority.java
@@ -1,5 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     ThreadPriority.java
+//  Description :   Demonstrates thread priority retrieval and
+//                  modification using Thread class methods.
+//  Author :        Varad Nitin Muley
+//  Date :          29/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    ThreadPriority
+//  Description :   Displays and modifies priority of the
+//                  currently executing thread.
+//
+/////////////////////////////////////////////////////////////////
+
 class ThreadPriority
 {
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Function Name : main
+    //  Description   : Entry point of Java application.
+    //  Input         : String array
+    //  Output        : void
+    //
+    /////////////////////////////////////////////////////////////////
+
     public static void main (String A[])
     {
         System.out.println("Inside Main method");
@@ -7,7 +34,7 @@ class ThreadPriority
         Thread tobj = Thread.currentThread();
         String name = tobj.getName();
         
-        System.out.println("Name of current thread is : "+ name);
+        System.out.println("Name of current thread is : " + name);
 
         int Priority = tobj.getPriority();
         System.out.println("Priority of current thread is : " + Priority);
@@ -16,6 +43,5 @@ class ThreadPriority
 
         Priority = tobj.getPriority();
         System.out.println("Priority of current thread is : " + Priority);
-
     }
 }
