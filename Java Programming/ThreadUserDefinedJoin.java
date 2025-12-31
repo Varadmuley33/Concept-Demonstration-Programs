53e587d4d217c64db653406536543dbd58cd209b
Varad Muley <varad.muley408@gmail.com>
2025-12-31 21:35:53 +0530

    Update ThreadUserDefinedJoin.java

--- Java Programming/ThreadUserDefinedJoin.java
+++ Java Programming/ThreadUserDefinedJoin.java
@@ -1,3 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     ThreadUserDefinedJoin.java
+//  Description :   Demonstrates creation of multiple user-defined
+//                  threads and assigning custom names to them.
+//  Author :        Varad Nitin Muley
+//  Date :          29/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    Demo
+//  Description :   User-defined thread class that prints
+//                  the name of the currently executing thread.
+//
+/////////////////////////////////////////////////////////////////
+
 class Demo extends Thread
 {
     public void run()
@@ -5,11 +23,30 @@ class Demo extends Thread
         System.out.println("Inside run method of : " + Thread.currentThread().getName());
     }
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    ThreadUserDefinedJoin
+//  Description :   Entry point class that creates threads,
+//                  assigns names, and starts execution.
+//
+/////////////////////////////////////////////////////////////////
+
 class ThreadUserDefinedJoin
 {
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Function Name : main
+    //  Description   : Entry point of Java application.
+    //  Input         : String array
+    //  Output        : void
+    //
+    /////////////////////////////////////////////////////////////////
+
     public static void main (String A[])
     {
         System.out.println("Inside Main thread");
+
         Demo dobj1 = new Demo();
         Demo dobj2 = new Demo();
 
