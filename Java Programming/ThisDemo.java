1ba90b458f8680114e372dd9b4e649225e99c1e7
Varad Muley <varad.muley408@gmail.com>
2025-12-31 21:32:40 +0530

    Update ThisDemo.java

--- Java Programming/ThisDemo.java
+++ Java Programming/ThisDemo.java
@@ -1,4 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     ThisDemo.java
+//  Description :   Demonstrates use of static block, constructor
+//                  chaining using this keyword, and instance
+//                  variable initialization in Java.
+//  Author :        Varad Nitin Muley
+//  Date :          29/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    Demo
+//  Description :   Contains static and instance variables,
+//                  constructors, and a display method.
+//
+/////////////////////////////////////////////////////////////////
 
 class Demo 
 {
@@ -9,29 +26,46 @@ class Demo
     {
         j = 21;
     }
+
     Demo ()
     {
         System.out.println("Inside Demo ");
         this.i = 11;
     }
+
     public Demo(int a )
     {
         this();
         System.out.println("Inside Parameterized ");
     }
+
     public void Display()
     {
         System.out.println("Inside display"+ this.i);
     }
-    
-
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    ThisDemo
+//  Description :   Entry point class of the application.
+//
+/////////////////////////////////////////////////////////////////
+
 class ThisDemo
 {
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Function Name : main
+    //  Description   : Entry point of Java application.
+    //  Input         : String array
+    //  Output        : void
+    //
+    /////////////////////////////////////////////////////////////////
+
     public static void main (String A[])
     {
         Demo dobj = new Demo();
         dobj.Display();
-
     }
 }
