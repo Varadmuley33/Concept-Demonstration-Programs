f0348f15e3c3ddba207673604543debe06bd1492
Varad Muley <varad.muley408@gmail.com>
2025-12-31 21:35:22 +0530

    Update ThreadUserDefined2.java

--- Java Programming/ThreadUserDefined2.java
+++ Java Programming/ThreadUserDefined2.java
@@ -1,3 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     ThreadUserDefined2.java
+//  Description :   Demonstrates execution of multiple user-defined
+//                  threads by creating more than one Thread object.
+//  Author :        Varad Nitin Muley
+//  Date :          29/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    Demo
+//  Description :   User-defined thread class extending Thread
+//                  with overridden run method.
+//
+/////////////////////////////////////////////////////////////////
+
 class Demo extends Thread
 {
     public void run()
@@ -5,11 +23,30 @@ class Demo extends Thread
         System.out.println("Inside run method ");
     }
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    ThreadUserDefined2
+//  Description :   Entry point class that creates and starts
+//                  multiple user-defined threads.
+//
+/////////////////////////////////////////////////////////////////
+
 class ThreadUserDefined2
 {
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Function Name : main
+    //  Description   : Entry point of Java application.
+    //  Input         : String array
+    //  Output        : void
+    //
+    /////////////////////////////////////////////////////////////////
+
     public static void main (String A[])
     {
         System.out.println("Inside Main thread");
+
         Demo dobj1 = new Demo();
         Demo dobj2 = new Demo();
 
