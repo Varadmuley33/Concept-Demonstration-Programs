6b6b393e75f664c474f8cbab2477dc33e836af4c
Varad Muley <varad.muley408@gmail.com>
2025-12-31 21:32:01 +0530

    Update SwitchDemo.java

--- Java Programming/SwitchDemo.java
+++ Java Programming/SwitchDemo.java
@@ -1,12 +1,40 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     SwitchDemo.java
+//  Description :   Demonstrates use of switch-case statement
+//                  to display exam timing based on standard.
+//  Author :        Varad Nitin Muley
+//  Date :          29/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    SwitchDemo
+//  Description :   Accepts standard from user and displays
+//                  corresponding exam time using switch-case.
+//
+/////////////////////////////////////////////////////////////////
+
 import java.util.Scanner;
+
 class SwitchDemo
 {
-   
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Function Name : main
+    //  Description   : Entry point of Java application.
+    //  Input         : String array
+    //  Output        : void
+    //
+    /////////////////////////////////////////////////////////////////
+
     public static void main (String A[])
     {
         Scanner sobj = new Scanner(System.in);
 
         int iStd = 0;
+
         System.out.println("Enter your Standard   : ");
         iStd = sobj.nextInt();
         
@@ -28,10 +56,8 @@ class SwitchDemo
                 System.out.println ("Exam is at 12 AM");
                 break;
 
-
-             default:
-                    System.out.println("Invalid input");
+            default:
+                System.out.println("Invalid input");
         }
-            
     }
 }
