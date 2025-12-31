e55883b4cca843cbe3c61eaf59a09367bcc58003
Varad Muley <varad.muley408@gmail.com>
2025-12-31 21:33:33 +0530

    Update ThreadDemo2.java

--- Java Programming/ThreadDemo2.java
+++ Java Programming/ThreadDemo2.java
@@ -1,5 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     ThreadDemo2.java
+//  Description :   Demonstrates retrieval of current thread
+//                  using Thread object and displays its name.
+//  Author :        Varad Nitin Muley
+//  Date :          29/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    ThreadDemo2
+//  Description :   Obtains current thread reference and
+//                  prints its name from main method.
+//
+/////////////////////////////////////////////////////////////////
+
 class ThreadDemo2
 {
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Function Name : main
+    //  Description   : Entry point of Java application.
+    //  Input         : String array
+    //  Output        : void
+    //
+    /////////////////////////////////////////////////////////////////
+
     public static void main (String A[])
     {
         System.out.println("Inside Main method");
@@ -7,6 +34,6 @@ class ThreadDemo2
         Thread tobj = Thread.currentThread();
         String name = tobj.getName();
         
-        System.out.println("Name of current thread is : "+ name);
+        System.out.println("Name of current thread is : " + name);
     }
 }
