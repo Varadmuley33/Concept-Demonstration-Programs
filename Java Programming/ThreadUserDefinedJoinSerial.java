4f2aa6da2d99264371a88510a67c3664d8cd18bd
Varad Muley <varad.muley408@gmail.com>
2025-12-31 21:37:14 +0530

    Add documentation to ThreadUserDefinedJoinSerial.java
    Added class and method documentation to improve code clarity.

--- Java Programming/ThreadUserDefinedJoinSerial.java
+++ Java Programming/ThreadUserDefinedJoinSerial.java
@@ -1,3 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     ThreadUserDefinedJoinSerial.java
+//  Description :   Demonstrates serial execution of user-defined
+//                  threads using join() method to control flow.
+//  Author :        Varad Nitin Muley
+//  Date :          29/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    Demo
+//  Description :   User-defined thread class that prints
+//                  the name of the currently executing thread.
+//
+/////////////////////////////////////////////////////////////////
+
 class Demo extends Thread
 {
     public void run()
@@ -5,31 +23,50 @@ class Demo extends Thread
         System.out.println("Inside run method of : " + Thread.currentThread().getName());
     }
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    ThreadUserDefinedJoinSerial
+//  Description :   Entry point class that executes threads
+//                  serially using join() method.
+//
+/////////////////////////////////////////////////////////////////
+
 class ThreadUserDefinedJoinSerial 
 {
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Function Name : main
+    //  Description   : Entry point of Java application.
+    //  Input         : String array
+    //  Output        : void
+    //
+    /////////////////////////////////////////////////////////////////
+
     public static void main (String A[]) 
     {
         System.out.println("Inside Main thread");
+
         Demo dobj1 = new Demo();
         Demo dobj2 = new Demo();
 
         dobj1.setName("First");
         dobj2.setName("Second");
 
-         // Checked Exception
+        // Checked Exception
         try
         {
-        dobj1.start();
-        dobj1.join();
-        System.out.println("End of first thread");
+            dobj1.start();
+            dobj1.join();
+            System.out.println("End of first thread");
 
-      
-        dobj2.start();
-        dobj2.join();
-        System.out.println("End of Second thread");
+            dobj2.start();
+            dobj2.join();
+            System.out.println("End of Second thread");
         }
         catch(InterruptedException iobj)
-        {}
+        {
+        }
 
         System.out.println("End of main thread");
     }
