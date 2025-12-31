46d975d8d05c426719a8c15f20a85c37f5063e21
Varad Muley <varad.muley408@gmail.com>
2025-12-31 21:33:07 +0530

    Update ThreadDemo1.java

--- Java Programming/ThreadDemo1.java
+++ Java Programming/ThreadDemo1.java
@@ -1,10 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     ThreadDemo1.java
+//  Description :   Demonstrates basic thread execution by
+//                  accessing the current thread and displaying
+//                  its name.
+//  Author :        Varad Nitin Muley
+//  Date :          29/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    ThreadDemo1
+//  Description :   Displays the name of the currently executing
+//                  thread from the main method.
+//
+/////////////////////////////////////////////////////////////////
+
 class ThreadDemo1
 {
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Function Name : main
+    //  Description   : Entry point of Java application.
+    //  Input         : String array
+    //  Output        : void
+    //
+    /////////////////////////////////////////////////////////////////
+
     public static void main (String A[])
     {
         System.out.println("Inside Main method");
 
         String name = Thread.currentThread().getName();
-        System.out.println("Name of current thread is : "+ name);
+        System.out.println("Name of current thread is : " + name);
     }
 }
