91f3d9f886b5836fb448fee96870652fd90fa112
Varad Muley <varad.muley408@gmail.com>
2026-01-01 21:12:49 +0530

    Update ThreadUserDefinedSynchronised.java

--- Java Programming/ThreadUserDefinedSynchronised.java
+++ Java Programming/ThreadUserDefinedSynchronised.java
@@ -1,3 +1,22 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     ThreadUserDefinedSynchronised.java
+//  Description :   Demonstrates table printing logic and basic
+//                  thread-related structure for synchronization
+//                  concept illustration.
+//  Author :        Varad Nitin Muley
+//  Date :          30/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    TablePrinter
+//  Description :   Provides functionality to print multiplication
+//                  table using current thread information.
+//
+/////////////////////////////////////////////////////////////////
+
 class TablePrinter
 {
     public void PrintTable(int n)
@@ -6,25 +25,49 @@ class TablePrinter
         
         for(i = 1 ; i <= 10 ; i++)
         {
-            System.out.println("Name : "+ Thread.currentThread().getName() + " : " + n*i);
+            System.out.println("Name : " + Thread.currentThread().getName() + " : " + n * i);
         }
     }
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    Demo
+//  Description :   User-defined thread class with empty run method.
+//
+/////////////////////////////////////////////////////////////////
+
 class Demo extends Thread
 {
     public void run()  
     {
-       
     }
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    ThreadUserDefinedSynchronised
+//  Description :   Entry point class that invokes table printing
+//                  methods sequentially.
+//
+/////////////////////////////////////////////////////////////////
+
 class ThreadUserDefinedSynchronised
 {
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Function Name : main
+    //  Description   : Entry point of Java application.
+    //  Input         : String array
+    //  Output        : void
+    //
+    /////////////////////////////////////////////////////////////////
+
     public static void main (String A[]) 
     {
         TablePrinter tobj = new TablePrinter();
 
         tobj.PrintTable(5);
         tobj.PrintTable(4);
-
     }
 }
