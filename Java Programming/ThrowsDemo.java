f827ff19f91e1fd7044c582c40397ea23435f151
Varad Muley <varad.muley408@gmail.com>
2026-01-01 21:15:15 +0530

    Enhance documentation in ThrowsDemo.java
    Added class and method documentation for clarity.

--- Java Programming/ThrowsDemo.java
+++ Java Programming/ThrowsDemo.java
@@ -1,3 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     ThrowsDemo.java
+//  Description :   Demonstrates exception propagation using
+//                  throws keyword and handling ArithmeticException.
+//  Author :        Varad Nitin Muley
+//  Date :          30/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    Demo
+//  Description :   Provides division operation which propagates
+//                  exception using throws keyword.
+//
+/////////////////////////////////////////////////////////////////
+
 class Demo 
 {
     public int Division(int No1 , int No2 ) throws ArithematicException
@@ -5,11 +23,28 @@ class Demo
         int iAns = 0 ;
         iAns = No1 / No2 ;
         return iAns ; 
-
     }
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name :    ThrowsDemo
+//  Description :   Entry point class that calls division method
+//                  and handles exception using try-catch.
+//
+/////////////////////////////////////////////////////////////////
+
 class ThrowsDemo
 {
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Function Name : main
+    //  Description   : Entry point of Java application.
+    //  Input         : String array
+    //  Output        : void
+    //
+    /////////////////////////////////////////////////////////////////
+
     public static void main (String A[])
     {
         Demo obj = new Demo ();
@@ -22,9 +57,9 @@ class ThrowsDemo
         }
         catch (ArithmeticException aobj)
         {
-            System.out.println("Inside catch "+aobj);
+            System.out.println("Inside catch " + aobj);
         }
-        System.out.println("Division is : " + Ret);
 
+        System.out.println("Division is : " + Ret);
     }
 }
