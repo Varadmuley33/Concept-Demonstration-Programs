f2f3f2eb26777fffebfd1fd0bfb68d7eec43946a
Varad Muley <varad.muley408@gmail.com>
2026-01-02 18:16:03 +0530

    Add files via upload

---
+++ Java Programming/AdditionClient3.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+class AdditionClient3
+{
+    public static void main (String A [])  throws Exception
+    {
+        System.out.println("Client is running ");
+
+
+        Socket sobj = new Socket("Localhost",2100);
+        System.out.println("Server Successfully connected  ");
+
+        DataInputStream diobj = new DataInputStream (sobj.getInputStream());
+        DataOutputStream doobj = new DataOutputStream(sobj.getOutputStream());
+
+        int No1 = 11 , No2 = 10;
+
+        doobj.writeInt(No1);
+        doobj.writeInt(No2);
+
+
+
+        int sum = diobj.readInt();
+        System.out.println("Addition of two numbers is : "+ sum);
+       
+        sobj.close();
+
+        System.out.println("Client application terminated.... ");
+    }
+}
+
+
+
+
