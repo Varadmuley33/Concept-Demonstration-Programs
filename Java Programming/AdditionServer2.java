7f26616ac3c135e886c72feaf338c6b29d1bd13d
Varad Muley <varad.muley408@gmail.com>
2026-01-02 18:16:52 +0530

    Add files via upload

---
+++ Java Programming/AdditionServer2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+class AdditionServer2
+{
+    public static void main (String A [])  throws Exception
+    {
+        System.out.println("Client is running ");
+        ServerSocket ssobj = new ServerSocket(2100);
+        System.out.println("Server is waiting at port number 2100");
+        Socket sobj = ssobj.accept();
+        System.out.println("Client request arrives and accepted by server ");
+
+        DataInputStream diobj = new DataInputStream (sobj.getInputStream());
+        DataOutputStream doobj = new DataOutputStream(sobj.getOutputStream());
+    }
+}
+
+
