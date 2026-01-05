08da912f5eef3ffcf8ce299797c4804edd083e40
Varad Muley <varad.muley408@gmail.com>
2026-01-05 23:18:04 +0530

    Add files via upload

---
+++ Java Programming/RMDDemo2.java
@@ -0,0 +1,64 @@
+class Base 
+{
+    public int i,j;
+    public void fun()
+    {
+        System.out.println("Inside Base fun");
+    }
+
+    public void gun()
+    {
+        System.out.println("Inside Base gun");
+    }
+
+    public void sun()
+    {
+        System.out.println("Inside Base sun");
+    }
+
+    public void bun()
+    {
+        System.out.println("Inside Base bun");
+    }
+
+}
+
+class Derived extends Base
+{
+    public int x;
+
+    public void gun()
+    {
+        System.out.println("Inside derived gun");
+    }
+
+     public void sun()
+    {
+        System.out.println("Inside derived sun");
+    }
+
+     public void run()
+    {
+        System.out.println("Inside derived run");
+    }
+
+     public void mun()
+    {
+        System.out.println("Inside derived mun");
+    }
+    
+}
+class RMDDemo2
+{
+    public static void main (String A[])
+    {
+        Base bp = new Derived ();         // Upcasting
+
+        bp.fun();    // Base fun
+        bp.gun();    // Derived gun
+        bp.sun();    // Derived sun
+        // bp.run();    // Error
+        // bp.mun();    // Error
+        bp.bun();    // Base bun
+    }   
+}
